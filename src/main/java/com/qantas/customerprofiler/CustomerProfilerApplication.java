c4cd7e73a793762717c67af17213aac47d742eed
ravi.sithiraju <ravi.sithiraju@healthdirect.org.au>
2018-07-31 22:15:17 +1000

    Initial commit

---
+++ src/main/java/com/qantas/customerprofiler/CustomerProfilerApplication.java
@@ -0,0 +1,12 @@
+package com.qantas.customerprofiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerProfilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerProfilerApplication.class, args);
+	}
+}
